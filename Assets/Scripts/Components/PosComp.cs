6b7e96a68a58a1c68c33f944fe54e6c41506bb0d
Unknown <1579058938@qq.com>
2021-03-01 18:42:53 +0800

    取消ECS框架

--- Assets/Scripts/Components/PosComp.cs
+++
@@ -1,6 +0,0 @@
-using Entitas;
-using UnityEngine;
-public class PosComp :IComponent
-{
-    public Vector2 value;
-}
--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -4,21 +4,5 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-   private GameSystems gameSystems;
     
-    private void Start()
-    {
-        Contexts contexts = Contexts.sharedInstance;
-        gameSystems = new GameSystems(contexts);
-        gameSystems.Initialize();
-    }
-    
-      void Update() {
-        gameSystems.Execute();
-        gameSystems.Cleanup();
-    }
-
-    void OnDestroy() {
-        gameSystems.TearDown();
-    }
 }
